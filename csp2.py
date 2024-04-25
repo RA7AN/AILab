b382fbec7071f00a3f787cafb755e1b7577928ec
RA7AN <rayyan20161@gmail.com>
2024-04-25 15:15:53 +0530

    Updated code

--- csp2.py
+++ csp2.py
@@ -1,3 +1,26 @@
+puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0], 
+		[6, 0, 0, 1, 9, 5, 0, 0, 0], 
+		[0, 9, 8, 0, 0, 0, 0, 6, 0], 
+		[8, 0, 0, 0, 6, 0, 0, 0, 3], 
+		[4, 0, 0, 8, 0, 3, 0, 0, 1], 
+		[7, 0, 0, 0, 2, 0, 0, 0, 6], 
+		[0, 6, 0, 0, 0, 0, 2, 8, 0], 
+		[0, 0, 0, 4, 1, 9, 0, 0, 5], 
+		[0, 0, 0, 0, 8, 0, 0, 0, 0] 
+		] 
+
+def print_sudoku(puzzle): 
+	for i in range(9): 
+		if i % 3 == 0 and i != 0: 
+			print("- - - - - - - - - - - ") 
+		for j in range(9): 
+			if j % 3 == 0 and j != 0: 
+				print(" | ", end="") 
+			print(puzzle[i][j], end=" ") 
+		print() 
+
+print_sudoku(puzzle) 
+
 class CSP: 
 	def __init__(self, variables, Domains,constraints): 
 		self.variables = variables 
@@ -36,26 +59,40 @@ class CSP:
 			if constraint_var in assignment and assignment[constraint_var] == value: 
 				return False
 		return True
+	
+	
+# Variables 
+variables = [(i, j) for i in range(9) for j in range(9)] 
+# Domains 
+Domains = {var: set(range(1, 10)) if puzzle[var[0]][var[1]] == 0
+						else {puzzle[var[0]][var[1]]} for var in variables} 
 
-puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0], 
-          [6, 0, 0, 1, 9, 5, 0, 0, 0], 
-          [0, 9, 8, 0, 0, 0, 0, 6, 0], 
-          [8, 0, 0, 0, 6, 0, 0, 0, 3], 
-          [4, 0, 0, 8, 0, 3, 0, 0, 1], 
-          [7, 0, 0, 0, 2, 0, 0, 0, 6], 
-          [0, 6, 0, 0, 0, 0, 2, 8, 0], 
-          [0, 0, 0, 4, 1, 9, 0, 0, 5], 
-          [0, 0, 0, 0, 8, 0, 0, 0, 0] 
-          ] 
-  
-def print_sudoku(puzzle): 
-    for i in range(9): 
-        if i % 3 == 0 and i != 0: 
-            print("- - - - - - - - - - - ") 
-        for j in range(9): 
-            if j % 3 == 0 and j != 0: 
-                print(" | ", end="") 
-            print(puzzle[i][j], end=" ") 
-        print() 
-  
-print_sudoku(puzzle)
+# Add contraint 
+def add_constraint(var): 
+	constraints[var] = [] 
+	for i in range(9): 
+		if i != var[0]: 
+			constraints[var].append((i, var[1])) 
+		if i != var[1]: 
+			constraints[var].append((var[0], i)) 
+	sub_i, sub_j = var[0] // 3, var[1] // 3
+	for i in range(sub_i * 3, (sub_i + 1) * 3): 
+		for j in range(sub_j * 3, (sub_j + 1) * 3): 
+			if (i, j) != var: 
+				constraints[var].append((i, j)) 
+# constraints		 
+constraints = {} 
+for i in range(9): 
+	for j in range(9): 
+		add_constraint((i, j)) 
+		
+# Solution 
+print('*'*7,'Solution','*'*7) 
+csp = CSP(variables, Domains, constraints) 
+sol = csp.solve() 
+
+solution = [[0 for i in range(9)] for i in range(9)] 
+for i,j in sol: 
+	solution[i][j]=sol[i,j] 
+	
+print_sudoku(solution)
